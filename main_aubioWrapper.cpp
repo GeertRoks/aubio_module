a74bec78c178db561e2c2bc05bf2e886d4a2c0f0
Geert Roks <geertrocks@gmail.com>
2019-12-05 12:32:18 +0100

    Added garbage collection

--- main_aubioWrapper.cpp
+++ main_aubioWrapper.cpp
@@ -8,8 +8,8 @@ int main() {
 
     float* tempbuffer = new float[aubio.getBuffersize()];
     for(unsigned int i = 0; i < aubio.getBuffersize(); i++) {
-        //tempbuffer[i] = (i * pow(2,i) + i*3)/ (0.5*i);
-        tempbuffer[i] = 0.0f;
+        tempbuffer[i] = (i * pow(2,i) + i*3)/ (0.5*i);
+        //tempbuffer[i] = 0.0f;
     }
     fvec_t* anabuffer;
     anabuffer = new_fvec(aubio.getBuffersize());
@@ -18,5 +18,8 @@ int main() {
 
     bool is_onset = aubio.process(anabuffer);
     std::cout << "is_onset: " << is_onset << std::endl;
+
+    del_fvec(anabuffer);
+    delete[] tempbuffer;
     return 0;
 }
