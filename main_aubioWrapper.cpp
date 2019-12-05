bb33ffddf6423e4d60cd98be4ad13b1d5029d1b1
Geert Roks <geertrocks@gmail.com>
2019-12-05 12:38:34 +0100

    End of First test - Added commments

--- main_aubioWrapper.cpp
+++ main_aubioWrapper.cpp
@@ -6,20 +6,25 @@ int main() {
 
     AubioWrapper aubio;
 
+    // Test signal initialisation (only one buffer in length)
     float* tempbuffer = new float[aubio.getBuffersize()];
     for(unsigned int i = 0; i < aubio.getBuffersize(); i++) {
-        tempbuffer[i] = (i * pow(2,i) + i*3)/ (0.5*i);
-        //tempbuffer[i] = 0.0f;
+        tempbuffer[i] = (i * pow(2,i) + i*3)/ (0.5*i);  // Buffer that gives an onset
+        //tempbuffer[i] = 0.0f;                         // Buffer that gives no onset
     }
+
+    // Initialize specialized type for Aubio and fill it with the test signal
     fvec_t* anabuffer;
     anabuffer = new_fvec(aubio.getBuffersize());
-
     anabuffer->data = tempbuffer;
 
+    // Run the buffer through Aubio and print the answer
     bool is_onset = aubio.process(anabuffer);
     std::cout << "is_onset: " << is_onset << std::endl;
 
+    // Garbage collection
     del_fvec(anabuffer);
     delete[] tempbuffer;
+
     return 0;
 }
