61c28577ece88c771a3672f660545996c9dab6d9
Geert Roks <geertrocks@gmail.com>
2020-02-27 16:40:07 +0100

    Fix aubio_module, new handling buffer- and hopsize
    In aubio_module I was still using the old way of buffersize and hopsize.
    I changed it so hopsize is the may way to interact.

--- src/aubio_module.cpp
+++ src/aubio_module.cpp
@@ -1,8 +1,8 @@
 #include "aubio_module.hpp"
 
 AubioModule::AubioModule() {
-    hopbuffer = new HopBuffer(this->buffersize, this->buffersize/this->hopsize);
-    input_fvec = new_fvec(this->buffersize);
+    hopbuffer = new HopBuffer(this->hopsize, this->hopfactor);
+    input_fvec = new_fvec(this->hopsize);
 }
 
 AubioModule::~AubioModule() {
@@ -18,9 +18,9 @@ float AubioModule::process(float* inputbuffer) {
 
 void AubioModule::updateAudioHandling() {
     delete hopbuffer;
-    hopbuffer = new HopBuffer(this->buffersize, this->hopfactor);
+    hopbuffer = new HopBuffer(this->hopsize, this->hopfactor);
     del_fvec(input_fvec);
-    input_fvec = new_fvec(this->buffersize);
+    input_fvec = new_fvec(this->hopsize);
     updateDetector();
 }
 
@@ -46,7 +46,7 @@ void AubioModule::setHopsize(unsigned int hopsize) {
     if (hopsize >= 8 && hopsize <= 8192) {
         if ((hopsize & (hopsize - 1)) == 0) {
             this->hopsize = hopsize;
-            this->hopfactor = this->buffersize/this->hopsize;
+            this->buffersize = this->hopfactor * this->hopsize;
             this->updateAudioHandling();
         } else {
             std::cout << "Error setHopsize: " << hopsize << " is not a valid hopsize. Choose either 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 or 8192 samples." << std::endl;
@@ -59,7 +59,7 @@ void AubioModule::setHopfactor(unsigned int hopfactor) {
     if (hopfactor <= 16 && hopfactor >= 1) {
         if (((hopfactor & (hopfactor - 1)) == 0) || hopfactor == 1 ) {
             this->hopfactor = hopfactor;
-            this->hopsize = this->buffersize/hopfactor;
+            this->buffersize = this->hopsize * this->hopfactor;
             this->updateAudioHandling();
         } else {
             std::cout << "Error setHopfactor: " << hopfactor << " is not a valid hopfactor. Choose either 1, 2, 4, 8 or 16." << std::endl;
